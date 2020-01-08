1d468f78d4e09a9aea3c0cdcc599cfd703e44409
Justin <jandersen770@gmail.com>
2020-01-08 00:36:07 -0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :user_name => "unionbasejob@gmail.com",
+      :password => "solidarity",
+      # :domain => "heroku.com",
+      :address => 'smtp.gmail.com',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true,
+    }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
