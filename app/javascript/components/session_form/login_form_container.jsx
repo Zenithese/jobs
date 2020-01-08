5e95dbb03944df2b29619435f52a12ea90a1e318
Justin <jandersen770@gmail.com>
2020-01-07 22:34:17 -0500

    cleanup

--- app/javascript/components/session_form/login_form_container.jsx
+++ app/javascript/components/session_form/login_form_container.jsx
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { login } from '../../actions/session_actions';
 import {  openModal, closeModal } from '../../actions/modal_actions'
 import SessionForm from './session_form';
